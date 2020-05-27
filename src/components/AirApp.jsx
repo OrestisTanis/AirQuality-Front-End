06ef18d44f2eeda51f2b8388ea1ef69234ecafa3
Orestis Tanis <tanisorestis@yahoo.com>
2020-05-27 20:07:47 +0300

    Set up project

---
+++ src/components/AirApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './navbar/navbar';
+import Footer from './footer/footer';
+
+function AirApp() {
+    return (
+        <>  
+            <Navbar></Navbar>
+            <h1>main</h1>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default AirApp;
+
+
+
+
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Navbar(){
+    return (
+        <p>navbar</p>
+    )
+}
+
+export default Navbar;
+
