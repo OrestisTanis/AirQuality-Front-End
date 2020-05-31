2d6775285c449195afb3d28607d5a85df544c19f
Orestis Tanis <tanisorestis@yahoo.com>
2020-06-01 00:28:17 +0300

    Added background-image for small screens in landing page

--- src/components/landing-page/background-video.jsx
+++ src/components/landing-page/background-video.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { isValidElement } from 'react';
 import poster from '../../shared/vid/cityPoster.png';
 import vid from '../../shared/vid/city.mp4';
 import './background-video.css'
@@ -10,6 +10,9 @@ function BackgroundVideo(props) {
             <video id="video-bg" poster={poster} autoPlay muted loop>
                 <source src={vid} type="video/mp4" />
             </video>
+            <div className="d-md-none">
+                <img id="image-poster" src={poster} alt="fresh air image"/>
+            </div>
             <div className="video-bg-overlay"></div>
             {/* SPACER */}
             <div id="spacer-100vh" style={{height: "100vh"}}></div>
