718088fd644be5918b410201c12eb03bd974002a
petros <pit.trak@gmail.com>
2020-06-02 16:04:59 +0300

    Modified Company Component

--- src/components/company/company-feature2.jsx
+++ src/components/company/company-feature2.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import img from './img/O1.png';
+import img from './img/tanis.png';
 
 function CompanyFeature2(props){
   return(
     <div className={props.className}>
-        <img src = {img}/>
-        <h5>Ορέστης Τάνης</h5>
-        <p>Ο χαρισματικός μας Ορέστης έχει κλήση στον προγραμματισμό. Μπορεί να
-           αφιερώνει ώρες στον κώδικα, ωστόσο χρειάζεται κάποιες ώρες να κλείνει τα
-           ματάκια του.</p>
+    <div className="mb-0 " style={{padding:'1rem', backgroundColor:'#EEEEEE', borderTopLeftRadius:'50%', borderTopRightRadius:'50%'}}>
+      <div className="shadow rounded-circle" style={{ width: '20rem', height: '20rem', overflow: 'none'}}>
+        <img className="rounded-circle" src={img} style={{ height: '100%', width: '100%', objectFit:'cover'}} />
+      </div>
+    </div>
+    <div className='pt-0 mt-0' style={{height:'20rem', backgroundColor:'#EEEEEE'}}>
+    <h5>Orestis Tanis</h5>
+    <p style={{textAlign: 'justify'}}>	Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
     </div>
   );
 }
--- src/components/company/company-feature3.jsx
+++ src/components/company/company-feature3.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import img from './img/P.png';
+import img from './img/petros.jpg';
 
 function CompanyFeature3(props){
   return(
     <div className={props.className}>
-        <img src = {img}/>
-        <h5>Πέτρος Τρακαδάς</h5>
-        <p>Ο Πέτρος γεννήθηκε στην Αθήνα και από μικρός
-           έδειξε το ενδιαφέρον του για νάνους και άλογα.</p>
+    <div className="mb-0" style={{padding:'1rem', backgroundColor:'#EEEEEE', borderTopLeftRadius:'50%', borderTopRightRadius:'50%'}}>
+      <div className="shadow rounded-circle" style={{ width: '20rem', height: '20rem', overflow: 'none'}}>
+        <img className="rounded-circle" src={img} style={{ height: '100%', width: '100%', objectFit:'cover'}} />
+      </div>
+    </div>
+    <div className='pt-0 mt-0' style={{height:'20rem', backgroundColor:'#EEEEEE'}}>
+    <h5>Petros Trakadas</h5>
+    <p style={{textAlign: 'justify'}}>	Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
     </div>
   );
 }
