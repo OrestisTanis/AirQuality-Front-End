052b33263552a987cdb522fb4540a2a963c75a54
Orestis Tanis <tanisorestis@yahoo.com>
2020-06-02 16:45:39 +0300

    Fixed company info to be responsive

--- src/components/company/company-feature.jsx
+++ src/components/company/company-feature.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import img from './img/panago.jpg';
+import './company.css';
 
 function CompanyFeature(props){
   return(
-    <div className={props.className}>
-    <div className="mb-0 " style={{padding:'1rem', backgroundColor:'#EEEEEE', borderTopLeftRadius:'50%', borderTopRightRadius:'50%'}}>
-      <div className="shadow rounded-circle" style={{ width: '20rem', height: '20rem', overflow: 'none'}}>
-        <img className="rounded-circle" src={img} style={{ height: '100%', width: '100%', objectFit:'cover'}} />
+    <div className="company-feature mb-5">
+      <div className="mb-0 " style={{padding:'1rem', backgroundColor:'#EEEEEE', borderTopLeftRadius:'50%', borderTopRightRadius:'50%'}}>
+        <div className="shadow rounded-circle" style={{ width: '20rem', height: '20rem', overflow: 'none'}}>
+          <img className="rounded-circle" src={img} style={{ height: '100%', width: '100%', objectFit:'cover'}} />
+        </div>
       </div>
-    </div>
-    <div className='pt-0 mt-0' style={{height:'20rem', backgroundColor:'#EEEEEE'}}>
-    <h5>Orestis Panagopoulos</h5>
-    <p style={{textAlign: 'justify'}}>	Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
+      <div className='pt-0 mt-0' style={{height:'20rem', backgroundColor:'#EEEEEE'}}>
+      <h5>Orestis Panagopoulos</h5>
+      <p className="pl-4 pr-4" style={{textAlign: 'justify'}}>	Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </div>
   );
 }
--- src/components/company/company.jsx
+++ src/components/company/company.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import CompanyFeature from './company-feature';
 import CompanyFeature2 from './company-feature2';
 import CompanyFeature3 from './company-feature3';
+import './company.css';
 
-function Company(){
-    return (
-      <>
-        <p>Company</p>
-        <div className ="container-fluid">
-          <div className = "container">
-            <div className="row text-center mt-5 mb-5">
-              <CompanyFeature className="col-12 col-md-4 mt-5 mb-5" />
-              <CompanyFeature2 className="col-12 col-md-4 mt-5 mb-5" />
-              <CompanyFeature3 className="col-12 col-md-4 mt-5 mb-5" />
-            </div>
-          </div>
+function Company() {
+  return (
+    <>
+      <p className="text-center" style={{paddingTop: '10rem'}}>Company</p>
+
+      <div className="container-fluid">
+        <div className="row text-center mt-5 mb-5 d-flex justify-content-center">
+          <CompanyFeature className="mt-5 mb-5" />
+          <CompanyFeature className="mt-5 mb-5" />
+          <CompanyFeature className="mt-5 mb-5" />
+          <CompanyFeature className="mt-5 mb-5" />
+
+
+          <CompanyFeature className="col-12 col-md-4 mt-5 mb-5" />
+          <CompanyFeature2 className="col-12 col-md-4 mt-5 mb-5" />
+          <CompanyFeature3 className="col-12 col-md-4 mt-5 mb-5" />
         </div>
-      </>
-    )
+      </div>
+
+    </>
+  )
 }
 
 export default Company;
