e147483e3d2602a3aa09d249768a4abbab31eb46
petros <pit.trak@gmail.com>
2020-06-04 21:34:16 +0300

    Fixed complete-payment

--- src/components/complete-payment/complete-payment.jsx
+++ src/components/complete-payment/complete-payment.jsx
@@ -5,39 +5,6 @@ import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios'
 
 function CompletePayment() {
-<<<<<<< Updated upstream
-
-  $(document).ready(function () {
-    $("#myModal").modal('show');
-  });
-
-  return (
-    <>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      <a href="#myModal" className="trigger-btn" data-toggle="modal"></a><br />
-      <div class="text-center">
-        <div style={{ paddingBottom: '10rem' }}>
-          <h4><a href="./">Continue to breath yourself healthy..</a></h4>
-        </div>
-      </div>
-      <div id="myModal" className="modal fade">
-        <div className="modal-dialog modal-confirm">
-          <div className="modal-content">
-            <div className="modal-header">
-              <div className="icon-box">
-                <i className="material-icons">&#xE876;</i>
-              </div>
-              <h4 className="modal-title">Awesome!</h4>
-            </div>
-            <div className="modal-body">
-              <p className="text-center">Your purchase has been confirmed. Check your email for detials.</p>
-            </div>
-            <div className="modal-footer">
-              <button className="btn btn-success btn-block" data-dismiss="modal">OK</button>
-            </div>
-          </div>
-        </div>
-=======
   $(document).ready(function(){
   $("#myModal").modal('show');
   });
@@ -51,7 +18,6 @@ function CompletePayment() {
   doPayment();
 
   function doPayment() {
-      // Go to the server || dispatch an action
 
       axios.post(`http://localhost:8080/paypal/complete/payment?paymentId=${paymentId}&payerId=${payerID}`)
           .then(res => {
@@ -61,9 +27,6 @@ function CompletePayment() {
               // Handle invalid credentials
               if (error.message) {
                   console.log(error.message);
-                  // const errors = {};
-                  // errors.invalidCredentials = "Invalid username or password.";
-                  // setErrors(errors);
               }
           })
   }
@@ -83,8 +46,18 @@ function CompletePayment() {
     <div className="modal-header">
       <div className="icon-box">
         <i className="material-icons">&#xE876;</i>
->>>>>>> Stashed changes
       </div>
+      <h4 className="modal-title">Awesome!</h4>
+    </div>
+    <div className="modal-body">
+      <p className="text-center">Your purchase has been confirmed. Check your email for detials.</p>
+    </div>
+    <div className="modal-footer">
+      <button className="btn btn-success btn-block" data-dismiss="modal">OK</button>
+    </div>
+  </div>
+</div>
+</div>
     </>
   );
 }
