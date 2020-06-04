7998e8da859f8234750e9dd196ee2a32e1b743fb
Orestis Tanis <tanisorestis@yahoo.com>
2020-06-04 16:12:40 +0300

    Ιncreased margin-bottom on main feature icons in landing-page

--- src/components/landing-page/main-feature.jsx
+++ src/components/landing-page/main-feature.jsx
@@ -8,19 +8,19 @@ function MainFeature(props) {
     return (
         <>
             <div className={props.className}>
-                <img className="mx-auto mb-2" src='https://www.iqair.com/assets/img/enterprise/air-api/il-world-coverage.svg' alt="world-wide coverage icon" />
+                <img className="mx-auto mb-4" src='https://www.iqair.com/assets/img/enterprise/air-api/il-world-coverage.svg' alt="world-wide coverage icon" />
                 <h5>Largest network of sensors</h5>
                 <p className="text-justify">AirVisual collects data from our huge network of ground-based sensors worldwide,
                providing the most accurate and reliable air quality information </p>
             </div>
             <div className={props.className}>
-                <img className="mx-auto mb-2" src="https://www.iqair.com/assets/img/enterprise/air-api/il-ai.svg" alt="artificial intelligence icon"/>
+                <img className="mx-auto mb-4" src="https://www.iqair.com/assets/img/enterprise/air-api/il-ai.svg" alt="artificial intelligence icon"/>
                 <h5>AI-driven data validation</h5>
                 <p className="text-justify">Only air quality data you can rely on. All data points are validated using
                 artificial intelligence and machine learning, to ensure data integrity. </p>
             </div>
             <div className={props.className}>
-                <img className="mx-auto mb-2" src='https://www.iqair.com/assets/img/enterprise/air-api/il-support.svg' alt="customer support icon" />
+                <img className="mx-auto mb-4" src='https://www.iqair.com/assets/img/enterprise/air-api/il-support.svg' alt="customer support icon" />
                 <h5>Premier customer support</h5>
                 <p className="text-justify">Our dedicated support team is on hand to help you quickly and seamlessly
                 install, register and manage your sensors within our application.</p>
