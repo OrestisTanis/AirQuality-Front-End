98af6f39fe2cfc271c7afb0a619fca463635dbfb
orestispanago <orestispanago@gmail.com>
2020-09-12 02:04:06 +0300

    Set nginx port 80

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker stop react
 git pull
 docker build -t react-app:latest .
-docker run -d --name react -it -p 88:80 --rm react-app:latest
+docker run -d --name react -it -p 80:80 --rm react-app:latest
